6051763e60c694b7e853140e87fa6ea47356e7c5
Valentina Acosta <valuchi005@gmail.com>
2023-11-05 16:17:01 -0300

    punto 10 terminado

--- js/10-ejercicio.js
+++ js/10-ejercicio.js
@@ -1,6 +1,6 @@
 const filas = parseInt(prompt("Introduce un numero de filas"));
 const columnas = parseInt(prompt("Introduce un numero de columnas"));
-const totalCeldas = filas * columnas;
+let totalCeldas = filas * columnas;
 
 document.write(`<table><tbody>`);
 for (let indiceFilas = 0; indiceFilas < filas; indiceFilas = indiceFilas + 1) {
@@ -10,7 +10,8 @@ for (let indiceFilas = 0; indiceFilas < filas; indiceFilas = indiceFilas + 1) {
     indiceColumnas < columnas;
     indiceColumnas = indiceColumnas + 1
   ) {
-    document.write(`<td>1</td>`);
+    document.write(`<td>${totalCeldas}</td>`);
+    totalCeldas = totalCeldas - 1;
   }
   document.write(`</tr>`);
 }
