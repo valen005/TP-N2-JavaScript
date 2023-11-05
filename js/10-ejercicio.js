8c64947c2795ba4205095579684e7b935442185e
Valentina Acosta <valuchi005@gmail.com>
2023-11-05 16:04:33 -0300

    Puede dibujar columnas

--- js/10-ejercicio.js
+++ js/10-ejercicio.js
@@ -1,15 +1,18 @@
 const filas = parseInt(prompt("Introduce un numero de filas"));
-const columna = parseInt(prompt("Introduce un numero de columnas"));
-const totalCeldas = filas * columna;
+const columnas = parseInt(prompt("Introduce un numero de columnas"));
+const totalCeldas = filas * columnas;
 
 document.write(`<table><tbody>`);
-for (let indiceFilas = 0; indiceFilas < filas; indiceFilas = indiceFilas + 1){
-    document.write(`
-<tr>
-    <td>
-        1
-    </td>
-</tr>`);
+for (let indiceFilas = 0; indiceFilas < filas; indiceFilas = indiceFilas + 1) {
+  document.write(`<tr>`);
+  for (
+    let indiceColumnas = 0;
+    indiceColumnas < columnas;
+    indiceColumnas = indiceColumnas + 1
+  ) {
+    document.write(`<td>1</td>`);
+  }
+  document.write(`</tr>`);
 }
-  
+
 document.write(`</tbody></table> `);
